41a5cb5801d71f1ce5cf348e1936830b3f7c4f07
Brenton Jackson <bjavonjackson@gmail.com>
2022-01-30 02:15:44 -0500

    validate text

--- server.js
+++ server.js
@@ -2,15 +2,7 @@ const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const extName = require('ext-name');
-const urlUtil = require('url');
-const axios = require('axios');
-
-const {TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER} = process.env;
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
-let twilioClient;
-
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -21,62 +13,54 @@ const PORT = process.env.PORT || 1337;
 let frontendPath = path.join(__dirname, 'frontend');
 
 app.use(express.static(frontendPath));
-
-function getTwilioClient() {
-    return twilioClient || new Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-}
-
-function deleteMediaItem(mediaItem) {
-    const client = getTwilioClient();
-
-    return client
-        .api.accounts(TWILIO_ACCOUNT_SID)
-        .messages(mediaItem.MessageSid)
-        .media(mediaItem.mediaSid).remove();
-}
-
-const supportedContent = ['image/jpeg','image/jpg','image/gif','image/png'];
-async function handleIncomingMMS(req, res){
-    const twiml = new MessagingResponse();
-    if (req.body.Body != "") {
-        twiml.message('Send us an image!');
-        return;
-    }
-    const { body } = req;
-    const mediaUrl = body['MediaUrl0'];
-    const contentType = body['MediaContentType0'];
-    let messageBody;
-    if (supportedContent.includes(contentType)) {
-        console.log(contentType);
-        messageBody = `Identification received`;
-    }
-    axios.get(`https://api.ocr.space/parse/imageurl?apikey=${process.env.OCR_API_KEY}&url=${mediaUrl}`)
-    .then(response => {
-        console.log(response.data);
-        twiml.message(response.data)
-    })
-    .catch(error => {
-        console.log(error);
-    });
-    res.send(response.toString()).status(200);
-}
-async function handleIncomingSMS(req, res) {
+app.post('/sms', (req, res) => {
     const twiml = new MessagingResponse();
     let message = req.body.Body;
     let regex = new RegExp('[a-zA-Z0-9]');
-    if (!regex.test(message)) {
-        twiml.message(req.body['MediaUrl0']);
+    if (regex.test(message)) {
+        twiml.message('Emojis only please 😎');
     } else {
-        twiml.message("Please send an image.")
-    }
-    
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-}
+        axios.get(`https://api.ocr.space/parse/imageurl?apikey=${process.env.OCR_API_KEY}&url=${mediaUrl}`)
+        .then(response => {
+            console.log(response.data);
+            twiml.message(response.data)
+        })
+        .catch(error => {
+            console.log(error);
+        });
+        res.send(response.toString()).status(200);
+        let randomNum = Math.round(Math.random());
+        let recipeName;
+        let recipeUrl;
 
-app.post('/mms', handleIncomingMMS);
-app.post('/sms', handleIncomingMMS);
+        if (randomNum == 1) {
+            recipeName = foodRecipes(message)[2];
+            recipeUrl = foodRecipes(message)[3];
+        } else {
+            recipeName = foodRecipes(message)[0];
+            recipeUrl = foodRecipes(message)[1];
+        }
+
+        if ((recipeName == undefined) || (recipeName == '')) {
+            twiml.message('Hi 😃! Thanks for using the demo version of EmojiRecipes! Contact Brenton to learn more about our full version!')
+        } else {
+            let convertedEmojis = [];
+            [...message].forEach(char => convertedEmojis.push(emojiDesc[char]))
+            twiml.message(`Grabbing your ${convertedEmojis.join(' ')} recipe!`);
+            twiml.message(recipeName)
+            twiml.message(recipeUrl)
+        }
+        
+    }
+
+
+
+  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.end(twiml.toString());
+});
 
 http.createServer(app).listen(PORT, () => {
-  console.log(`Express server listening on port ${PORT}`);
+  console.log('Express server listening on port 1337');
 });
+
+
