1978d20b9bec0c8c6e2920167e6eb5f8eee920d5
Brenton Jackson <bjavonjackson@gmail.com>
2022-01-29 19:10:02 -0500

    update image processing

--- veinDriver.py
+++ veinDriver.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 import cv2
 
-# c is percent of image filled
-def cropCenter(img, c):
+maxValue = 150
+cropSize = 4
+
+def getCenter(img, c):
     c = c / 100
     h, w, _ = img.shape
     cw = c*w
@@ -12,31 +14,40 @@ def cropCenter(img, c):
     w2 = (w + cw)/2
     h1 = (h - ch)/2
     h2 = (h + ch)/2
+    return int(w1), int(w2), int(h1), int(h2)
+
+# c is percent of image filled
+def cropCenter(img, c):
+    w1, w2, h1, h2 = getCenter(img, c)
     return img[int(h1):int(h2), int(w1):int(w2)]
 
-def helper(img):
-    img = cropCenter(img, 10)
+def processImage(img):
+    img = cropCenter(img, cropSize)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cv2.imshow('cropped', img)
     mean = cv2.mean(img)
     
-    print(mean)
-#    if (//stuff):
- #       return true
-  #else return false
-    return img
 
-cap = cv2.VideoCapture(0) #cv2.VideoCapture('http://172.22.50.203:8080/video');
+    value = mean[2]
+    print(value)
+    isVein = value < maxValue
+    #print(isVein)
+    return isVein
+
+cap = cv2.VideoCapture('http://172.22.50.203:8080/video')
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    cropped = helper(frame)
+    processImage(frame)
     # Our operations on the frame come here
 #    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    w1, w2, h1, h2 = getCenter(frame, cropSize)
+    frame = cv2.rectangle(frame, (w1, h1), (w2, h2), (0, 255, 0), 2)
+
     # Display the resulting frame
     cv2.imshow('window', frame)
-    cv2.imshow('cropped', cropped)
     if cv2.waitKey(1) & 0xFF == ord('q'):
        break
 
